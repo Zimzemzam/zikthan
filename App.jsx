0b286853a6757e3f8057348fe7aa5141c5b13886
Zimzemzam <amoskhiangte96@gmail.com>
2025-06-10 13:51:34 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 text-gray-900 p-4">
+      <h1 className="text-4xl font-bold text-center mb-8">Bawk Zam</h1>
+      <p className="text-center">Your game component will be rendered here.</p>
+    </div>
+  );
+}
